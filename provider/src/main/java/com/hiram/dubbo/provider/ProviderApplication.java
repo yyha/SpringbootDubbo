11234f2bed59388e0950f3ce1512bc03d30f4754
yyha <y997090615@163.com>
2019-07-25 18:04:59 +0800

    rabbitmq

--- provider/src/main/java/com/hiram/dubbo/provider/ProviderApplication.java
+++ provider/src/main/java/com/hiram/dubbo/provider/ProviderApplication.java
@@ -3,10 +3,18 @@ package com.hiram.dubbo.provider;
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @EnableDubbo
 @SpringBootApplication
-public class ProviderApplication {
+public class ProviderApplication  {
+
+
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(ProviderApplication.class);
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProviderApplication.class, args);
--- provider/src/main/java/com/hiram/dubbo/provider/service/impl/DemoServiceImpl.java
+++ provider/src/main/java/com/hiram/dubbo/provider/service/impl/DemoServiceImpl.java
@@ -2,14 +2,24 @@ package com.hiram.dubbo.provider.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.hiram.dubbo.api.service.DemoService;
+import com.hiram.dubbo.provider.rabbitmq.MsgProducer;
 import com.hiran.dubbo.entity.User;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service(version = "${demo.service.version}")
 public class DemoServiceImpl implements DemoService {
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
     @Override
     public String sayHello(String name) {
-        return "Hello " + name + ", I'm provider!";
+
+        MsgProducer msgProducer = new MsgProducer(rabbitTemplate);
+        msgProducer.sendMsg("我是贾柏林");
+
+        return "Hello " + name + ", I'm provider!111";
     }
 
     @Override
